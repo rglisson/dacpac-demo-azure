838b4efe3f74827674074d71e26b6ebcd1ccf910
rglisson <rjg8b8@gmail.com>
2022-08-13 11:15:52 -0500

    add database project

---
+++ DatabaseProjects/PortalDatabase/Objects/dbo/Tables/Demographics.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE EmployeeDemographics (
+	EmployeeID int, 
+	FirstName varchar(50), 
+	LastName varchar(50), 
+	Age int, 
+	Gender varchar(50)
+)
---
+++ DatabaseProjects/PortalDatabase/Objects/dbo/Tables/EmployeeSalary.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE EmployeeSalary (
+	EmployeeID int, 
+	JobTitle varchar(50), 
+	Salary int
+)
